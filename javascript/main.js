ca0f1f80af5b0e0147cacec864b6299bab098624
Nicholas Glover <jacksaintjack@gmail.com>
2015-10-29 21:15:52 -0500

    added the footer, but having a problem with parsing out JSON data to match the objects by ID.

--- javascript/main.js
+++ javascript/main.js
@@ -15,9 +15,24 @@ $.ajax("https://api.github.com/users/jacksaintjack").done(function(data){
 
 var dataRepo = $.ajax("https://api.github.com/users/jacksaintjack/repos").done(function(data){
 
-  $.each(data, function(i, add){
-     $("#projectArea").append('<li>'+'<a href ='+ add.html_url +'>' + add.name + '</a>' + '</li>');
+  $.each(data, function(repos, add){
+     $("#projectArea").append('<div id="projectBox"><li>'+'<a href ='+ add.html_url +'>' + add.name + '</a>' +
+     '<li id=stats></li>' + '</li></div>');
+
+    //  if(projectName === projectCode){
+    //    $('#stats').append(add.language);
+    //  }
 })
+
+function getColumnIndex(id, key) {
+    return columns.indexOf(key)
+}
+
+var nameIndex = getColumnIndex(jsonSource.COLUMNS, 'NAME');
+
+$.each(jsonSource.DATA, function( indexInArray, value ) {
+    console.log( value[nameIndex] );
+});
 
 
 });
