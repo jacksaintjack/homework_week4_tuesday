1c13f2834586216369c9fa768c05e719fee33793
Nicholas Glover <jacksaintjack@gmail.com>
2015-11-01 13:54:35 -0600

    added time for the profile and used moment.js on them

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,7 @@ $.ajax("https://api.github.com/users/jacksaintjack").done(function(data){
     $('#userName').text(data.login);
     $('#company').text(data.company);
     $('#location').text(data.location);
-    $('#dateJoined').text(data.created_at);
+    $('#dateJoined').text(moment(data.created_at).format('ll'));
     $('#followers').text(data.followers);
     $('#following').text(data.following);
     $("#followers").append('<br><a href ='+ data.followers_url +'>' + 'Follower </a>');
@@ -14,10 +14,11 @@ $.ajax("https://api.github.com/users/jacksaintjack").done(function(data){
   });
 
 var dataRepo = $.ajax("https://api.github.com/users/jacksaintjack/repos").done(function(data){
-  $.each(data, function(repos, add){
+    $.each(data, function(repos, add){
      $("#projectArea").append('<div id="projectBox"><li>'+'<a href ='+ add.html_url +'>' + add.name + '</a>' +
      '<li id="stats">'+ add.language + '<span class="octicon octicon-star starIcon"></span>' + '<span class=>'+ add.stargazers_count +
-     '</span>' + '<span class="octicon octicon-git-branch branchIcon"></span>' + '<span class=>' + add.forks_count + '</span>' + '</li>' + '</li></div>');
+     '</span>' + '<span class="octicon octicon-git-branch branchIcon"></span>' + '<span class=>' + add.forks_count + '</span>' + '</li>' +
+     '<span class="updatedTime"> updated '+ moment(add.updated_at).fromNow() +'</span>'  + '</li></div>');
 
 })
 
